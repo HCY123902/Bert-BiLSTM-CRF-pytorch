4f4b6d684045e18643d0e9f5d1f0eb5bc9869f81
hcy <2320032466@qq.com>
2021-11-09 19:19:54 +0800

    Adjust the character

--- main.py
+++ main.py
@@ -49,8 +49,10 @@ def train(model, iterator, optimizer, criterion, device):
         if i%10==0: # monitoring
             print(f"step: {i}, loss: {loss.item()}")
 
-def eval(model, iterator, f, device, epoch):
+def eval(model, iterator, f, device):
     model.eval()
+
+    path = "{}_prediction".format(f)
 
     Words, Is_heads, Tags, Y, Y_hat = [], [], [], [], []
     with torch.no_grad():
@@ -68,7 +70,7 @@ def eval(model, iterator, f, device, epoch):
             Y_hat.extend(y_hat.cpu().numpy().tolist())
 
     ## gets results and save
-    with open("temp_{}".format(epoch), 'w', encoding='utf-8') as fout:
+    with open(path, 'w', encoding='utf-8') as fout:
         for words, is_heads, tags, y_hat in zip(Words, Is_heads, Tags, Y_hat):
             y_hat = [hat for head, hat in zip(is_heads, y_hat) if head == 1]
             preds = [idx2tag[hat] for hat in y_hat]
@@ -80,8 +82,8 @@ def eval(model, iterator, f, device, epoch):
             fout.write("\n")
 
     ## calc metric
-    y_true =  np.array([tag2idx[line.split()[1]] for line in open("temp_{}".format(epoch), 'r', encoding='utf-8').read().splitlines() if len(line) > 0])
-    y_pred =  np.array([tag2idx[line.split()[2]] for line in open("temp_{}".format(epoch), 'r', encoding='utf-8').read().splitlines() if len(line) > 0])
+    y_true =  np.array([tag2idx[line.split()[1]] for line in open(path, 'r', encoding='utf-8').read().splitlines() if len(line) > 0])
+    y_pred =  np.array([tag2idx[line.split()[2]] for line in open(path, 'r', encoding='utf-8').read().splitlines() if len(line) > 0])
 
     num_proposed = len(y_pred[y_pred>1])
     num_correct = (np.logical_and(y_true==y_pred, y_true>1)).astype(np.int).sum()
@@ -110,7 +112,7 @@ def eval(model, iterator, f, device, epoch):
 
     final = f + ".P%.2f_R%.2f_F%.2f" %(precision, recall, f1)
     with open(final, 'w', encoding='utf-8') as fout:
-        result = open("temp", "r", encoding='utf-8').read()
+        result = open(path, "r", encoding='utf-8').read()
         fout.write(f"{result}\n")
 
         fout.write(f"precision={precision}\n")
@@ -202,7 +204,7 @@ if __name__=="__main__":
         print(f"=========eval at epoch={epoch}=========")
         if not os.path.exists(hp.logdir): os.makedirs(hp.logdir)
         fname = os.path.join(hp.logdir, str(epoch))
-        precision, recall, f1 = eval(model, eval_iter, fname, device, epoch)
+        precision, recall, f1 = eval(model, eval_iter, fname, device)
 
         torch.save(model.state_dict(), f"{fname}.pt")
         print(f"weights were saved to {fname}.pt")
